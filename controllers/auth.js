081f34988bafaa97adeb5eb172c3b2d42a9aab09
thefallxo96 <titodelvalle824@gmail.com>
2025-09-16 18:51:50 -0400

    here we are

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/users');
 
 // Show register form
 router.get('/register', (req, res) => res.render('auth/register'));
